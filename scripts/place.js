1a60d38e7b4e7ef5ea9779574496373c7cd82170
Psyduck2002 <kroper1@byupathway.edu>
2026-07-18 13:49:35 -0700

    finished places assignment

--- scripts/place.js
+++ scripts/place.js
@@ -7,8 +7,20 @@ document.getElementById("lastModified").textContent =
 
 function calculateWindChill(temp, speed) {
     if (temp <= 50 && speed > 3) {
-        const windChill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
-        return windChill;
+        const windChill =
+            35.74 +
+            (0.6215 * temp) -
+            (35.75 * Math.pow(speed, 0.16)) +
+            (0.4275 * temp * Math.pow(speed, 0.16));
+
+        return `${windChill.toFixed(1)} °F`;
     }
+
     return "N/A";
 }
+
+const temperature = 90;
+const windSpeed = 6;
+
+document.getElementById("windChill").textContent =
+    calculateWindChill(temperature, windSpeed);
